08bd10141d21613525159923ed4d8e0617a48fb6
jerichonaces <nacesjericho@gmail.com>
2023-03-09 00:23:29 +0800

    deployment v11

--- server/index.js
+++ server/index.js
@@ -31,6 +31,15 @@ app.use(
 // Add the Access-Control-Allow-Origin header to all responses
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 

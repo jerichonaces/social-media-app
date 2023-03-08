c7fe942158de4460416c0a0b8ad2b2f6811372ad
jerichonaces <nacesjericho@gmail.com>
2023-03-08 16:57:31 +0800

    deployment v7

--- server/index.js
+++ server/index.js
@@ -23,14 +23,15 @@ mongoose
   .then(() => console.log('DB CONNECTED'))
   .catch((err) => console.log('DB CONNECTION ERROR => ', err));
 
-// middlewares
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
   })
 );
+
+// middlewares
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 // autoload routes
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
